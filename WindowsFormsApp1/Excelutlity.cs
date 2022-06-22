6a1dce0dba0f811e08761028052704aed0dc3a3e
ahmed3noman <Ahmed.Noman@awico.com>
2022-06-22 06:24:08 +0200

    Ich habe das Problem des Speicherortes von der Excelliste gelöst!

--- WindowsFormsApp1/Excelutlity.cs
+++ WindowsFormsApp1/Excelutlity.cs
@@ -55,7 +55,7 @@ namespace WittEyE
                 {
                     try
                     {
-                        excelFilePath += "_" + DateTime.Now.ToShortDateString() + ".xlsx";
+                        excelFilePath += "\\Offne Besellungen_" + DateTime.Now.ToShortDateString() + ".xlsx";
                         workSheet.SaveAs(excelFilePath);
                         excelApp.Quit();
                         MessageBox.Show("Excelliste ist gespeichert");
